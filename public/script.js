f063f9738a5bba3c5edb9ea5930a7a77d8702ae0
Danielle Oliveira <danielle.o.n@hotmail.com>
2021-04-16 22:35:32 -0300

    quase lá

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
-// const cards = require("../data");
-
+const cards = document.querySelectorAll(".card")
 
 // for (const card of cards) {
 //     card.addEventListener("click",(evt)=>{
+
 //         // const img = evt.currentTarget.querySelector("img").src;
 //         // modalPequeno.querySelector("h6").textContent = evt.currentTarget.querySelector("h6").textContent;
 //         // modalPequeno.querySelector("p").textContent = evt.currentTarget.querySelector("p").textContent;
 //         // console.log(evt.currentTarget);
         
-        
-//         console.log(card);
 //     })   
 // }
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener("click",(evt)=>{
+
+        // const img = evt.currentTarget.querySelector("img").src;
+        // modalPequeno.querySelector("h6").textContent = evt.currentTarget.querySelector("h6").textContent;
+        // modalPequeno.querySelector("p").textContent = evt.currentTarget.querySelector("p").textContent;
+        // console.log(evt.currentTarget);
+        const index = i;
+        window.location.href = `/recipes/${index}`;
+        
+        console.log(i);
+    })
+}
